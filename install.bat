84e86d870f3aa388df273d96572710699192f056
Juan José <juanjoseba98@gmail.com>
2023-06-19 20:02:00 -0500

    feat: update install bat to ask and install node depencies

--- install.bat
+++ install.bat
@@ -2,6 +2,18 @@
 echo ----------------------------------------------
 echo ------------Instalando herramienta------------
 echo ----------------------------------------------
+echo ------Antes de instalar mueva la carpeta------
+echo -------------a su ubicacion final-------------
+echo ----------------------------------------------
+set /P isReadyToInstall=Seguro de instalar? Y/N 
+IF /i %isReadyToInstall%==Y (
+    goto install
+)
+exit
+:install
+echo ----------------------------------------------
+echo -------Instalando dependencias de node--------
+npm i
 set desktopFolder=%USERPROFILE%\Desktop
 set desktopFolderOneDrive="%USERPROFILE%\OneDrive - Telefonica\Desktop"
 set infolaftToolBat=infolaftTool.bat
@@ -9,19 +21,16 @@ set toolFolder=%~dp0
 set command=.\node.exe .\index.js --nombre NOMBRE.xlsx --nombreId NOMBRE_ID.xlsx --outFile Resultados
 echo ----------------------------------------------
 echo ------Eliminando installaciones previas-------
-echo ----------------------------------------------
 del %desktopFolder%\%infolaftToolBat%
 del %desktopFolderOneDrive%\%infolaftToolBat%
 echo ----------------------------------------------
 echo ---------------Creando archivos---------------
-echo ----------------------------------------------
 echo cd %toolFolder% >> %desktopFolder%\%infolaftToolBat%
 echo %command% >> %desktopFolder%\%infolaftToolBat%
 echo cd %toolFolder% >> %desktopFolderOneDrive%\%infolaftToolBat%
 echo %command% >> %desktopFolderOneDrive%\%infolaftToolBat%
 echo ----------------------------------------------
 echo -------Creando carpeta de procesado-----------
-echo ----------------------------------------------
 cd ..
 rmdir /S /Q filesToProcess
 mkdir filesToProcess
